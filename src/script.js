34b4fb1ecc56f58f80e5d9070049ef7ed479e5e2
Dhaesha M <dhaesha1@gmail.com>
2022-04-08 23:23:10 -0400

    mesh scale

--- src/script.js
+++ src/script.js
@@ -23,7 +23,12 @@ scene.add(arrowHelper);
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
 const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.x = 2;
+mesh.scale.y = 0.25;
+mesh.scale.z = 0.5;
+
 scene.add(mesh);
 
 /**
